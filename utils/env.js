957dd2cd429ddd7f7d869e3e789a6a21c5839e6f
Remy Wood <99709675+remwood@users.noreply.github.com>
2022-10-30 16:56:33 -0700

    Add Redirect URI

--- utils/env.js
+++ utils/env.js
@@ -2,7 +2,7 @@ import { Platform } from "react-native";
 
 // ***** TODO: Fill in your constants here ***** //
 const CLIENT_ID = "6f55795e14bb4d8d8386a5c82511c7c5";
-const REDIRECT_URI = "exp://127.0.0.1:19000"; // TODO: Replace this with your own redirect URI
+const REDIRECT_URI = "exp://q3-dp8.anonymous.cs47-a3.exp.direct:80"; // TODO: Replace this with your own redirect URI
 const ALBUM_ID = "2nLOHgzXzwFEpl62zAgCEC?si=92gvi84ZTLKfMD-YgYWAUw"; // By default, this is the Weeknd's Album's "DAWN FM"
 // ********************************************* //
 
