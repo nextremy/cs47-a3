dd8cc1166b4ea5014be28ec296d5b83706c4fe33
Remy Wood <99709675+remwood@users.noreply.github.com>
2022-10-30 17:45:32 -0700

    Pull top tracks instead of album

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { useSpotifyAuth } from "./utils";
 
 export default function App() {
   // Pass in true to useSpotifyAuth to use the album ID (in env.js) instead of top tracks
-  const { token, tracks, getSpotifyAuth } = useSpotifyAuth(true);
+  const { token, tracks, getSpotifyAuth } = useSpotifyAuth();
 
   return (
     <SafeAreaView style={styles.container}>
