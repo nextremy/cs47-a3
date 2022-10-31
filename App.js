acc5fb7a73768e9dacd6a1d77881bfa7bb97fba7
Remy Wood <99709675+remwood@users.noreply.github.com>
2022-10-30 17:19:40 -0700

    Organize imports

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { StyleSheet, SafeAreaView, Text } from "react-native";
-import { useSpotifyAuth } from "./utils";
+import { SafeAreaView, StyleSheet } from "react-native";
 import { Themes } from "./assets/Themes";
 import SpotifyAuthButton from "./components/SpotifyAuthButton";
+import { useSpotifyAuth } from "./utils";
 
 export default function App() {
   // Pass in true to useSpotifyAuth to use the album ID (in env.js) instead of top tracks
